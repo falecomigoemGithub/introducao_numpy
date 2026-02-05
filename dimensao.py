c567cfa12e53b5a2eee33eb10f4af4a85b6780d1
Angelo aka Frost <falecomigoem@gmail.com>
2026-02-05 11:05:38 -0300

    Inclusao do arquivo indexacao.py

--- dimensao.py
+++ dimensao.py
@@ -2,5 +2,7 @@ import numpy as np
 
 array_1d = np.array([1, 2, 3])
 array_2d = np.array([[4, 5, 6], [7, 8, 0]])
-print(array_1d.shape)
-print(array_2d.shape)
+array_3d = np.array([[[1, 2], [3, 4], [5, 6], [7, 8]]])
+array_4d = np.zeros((2, 3, 4, 3))
+
+print(array_4d)
