9f4afc0a9ff31039fe55e158fc64650062b8be18
Angelo aka Frost <falecomigoem@gmail.com>
2026-02-19 12:47:49 -0300

    fim da aula

--- iteracao.py
+++ iteracao.py
@@ -1,7 +1,26 @@
 import numpy as np
+import itertools
 
 matriz = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print("#", 20)
-print(matriz)
+
+print(type(matriz))
 for mt in matriz:
-    print(mt)
+    # print(mt)
+    pass
+
+for linha in matriz:
+    for valor in linha:
+        pass
+for l in itertools.product(matriz):
+    pass
+for valor in np.nditer(matriz):
+    pass
+
+for i in range(matriz.shape[0]):
+    for j in range(matriz.shape[1]):
+        # print(f"Elementos na posição -> ({i}, {j}): {matriz[i, j]}")
+        pass
+
+for linha in matriz:
+    print("Retorno do shape ", linha.shape)
+    print("Retorno do ndim ", linha.ndim)
