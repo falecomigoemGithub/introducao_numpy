2d92eca95b3c481cc3635534fe9e0d4b9d83768c
Angelo aka Frost <falecomigoem@gmail.com>
2026-02-20 12:00:13 -0300

    fim da aula revisão

--- revisao.py
+++ revisao.py
@@ -2,5 +2,7 @@ import numpy as np
 
 nomes = np.array(["Alice", "Bob", "Charlie", "Diana", "Emma"])
 
-grupos = np.split(nomes, 3)
-print(grupos)
+grupos = np.array_split(nomes, 3)
+
+for i, sub_grupo in enumerate(grupos):
+    print(f"Grupo {i + 1}: {sub_grupo}")
